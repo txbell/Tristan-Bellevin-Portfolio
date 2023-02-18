beb644d2181ba355992b2738758f13ff1de754b9
txbell <tristan@bellevin.com>
2023-02-17 21:37:32 -0800

    added my pfp, starting to set up boxes around the image

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+// IDEA: Auto generate the boxes of color around the elements in the home.html
+// How it would work: I preset the possible column and row values for the grid-row and grid-column positions,
+// then I create an empty div, to which I add a randomly selected column and row value,
+//  as well as a random background color (maybe from a preset collection)
+
+// array of possible column values for each box
+const boxCArr = []
+// array of possible row values for each box
+const boxRArr = []
+
+// function that 
