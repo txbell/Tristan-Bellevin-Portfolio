8b623a27f9110e82562b828f0ef7e2f77c4872b6
txbell <tristan@bellevin.com>
2023-02-17 23:23:05 -0800

    added change in pfp based on smaller than 700px or larger than 1000px

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// setting up media queries to check width dimentions of screen
-
-
 // IDEA: Auto generate the boxes of color around the elements in the home.html
 // How it would work: I preset the possible column and row values for the grid-row and grid-column positions,
 // then I create an empty div, to which I add a randomly selected column and row value,
@@ -9,12 +6,24 @@
 // array of objects that contain possible row and column values for each box
 const boxArr = [{row: "1/ span 5", column: "1/span 3",}, {row: "1/span 2", column: "6/span 3",}, {row: "1/span 4", column: "9/span 2",}
     , {row: "5/span 3", column: "7/span 4",}, {row: "8/span 3", column: "8/span 3",}, {row: "9/span 2", column: "4/span 4",}
-    , {row: "8/span 3", column: "1/span 3",}, {row: "6/span 2", column: "1/span 2",}, {row: "1/span 2", column: "4/span 2",}, {row: "3/span 2", column: "6/span 1",}
-    , {row: "5/span 1", column: "4/span 1",}, ]
+    , {row: "8/span 3", column: "1/span 3",}, {row: "6/span 2", column: "1/span 2",}, {row: "1/span 2", column: "4/span 2",}
+    , {row: "3/span 2", column: "6/span 1",}, {row: "5/span 1", column: "4/span 1",}, ]
 const bckgrndColor = ["#582f0e", "#7f4f24", "#936639", "#a68a64", "#b6ad90", "#c2c5aa", "#a4ac86", "#656d4a", "#414833", "#333d29"]
 
+if (window.innerWidth > 600 && window.innerWidth < 1000) {
+    document.getElementById('txbImg').style.height = "30vh";
+    document.getElementById('txbImg').style.gridRowStart = "5";
+} else if (window.innerWidth > 1000) {
+    document.getElementById('txbImg').style.height = "40vh";
+    document.getElementById('txbImg').style.gridRowStart = "4";
+    document.getElementById('resume').style.gridRow = "2/span 2";
+    boxArr[8].row = "1/span 1"
+    boxArr[9].row = "3/span 1"
+    boxArr[10].row = "4/span 2"
+} else if (window.innerWidth < 600) {
+    boxArr[10].column = "4/span 3"
+}
 
-console.log(Math.floor(Math.random() * bckgrndColor.length))
 // function that creates an empty div with an id of "box " + its number, then adds a grid position, 
 // then adds a background color, then adds it as a child of the body.
 function makeDiv (x) {
