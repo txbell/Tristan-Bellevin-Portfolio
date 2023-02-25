a03ca9a83641431c4856b83a9b09a9d149cb0a4f
txbell <tristan@bellevin.com>
2023-02-24 16:10:53 -0800

    4:10px 2/24/23

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// IDEA: Auto generate the boxes of color around the elements in the home.html
+// IDEA: Auto generate the boxes of color around the elements in the index.html
 // How it would work: I preset the possible column and row values for the grid-row and grid-column positions,
 // then I create an empty div, to which I add a randomly selected column and row value,
 //  as well as a random background color (maybe from a preset collection)
@@ -8,7 +8,7 @@ console.log(window.location.pathname)
 if (sessionStorage.getItem('number') === null) {
     sessionStorage.setItem('number', '0')
 }
-if (window.location.pathname === "/Tristan-Bellevin-Portfolio/home.html") {
+if (window.location.pathname === "/Tristan-Bellevin-Portfolio/index.html") {
 
 // creating array of grid space I want the boxes to take up
 const boxArr = [ {row: "1/ span 5", column: "1/span 3",}, {row: "1/ span 1", column: "4/span 2",}, 
